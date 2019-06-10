7328e4b07f12d295f4488dbcf9c0b9000b330fd4
github username <github email address>
2019-06-10 16:32:06 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
+    self.new
     @post.title = params[:title]
     @post.description = params[:description]
     @post.save
